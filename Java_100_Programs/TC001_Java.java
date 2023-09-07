3545fab9c504e21686f416af2a633dfde1f2e999
Keyur <keyurshah12345@gmail.com>
2023-09-07 07:32:10 +0530

    1st program

---
+++ Java_100_Programs/TC001_Java.java
@@ -0,0 +1,10 @@
+package Java_100_Programs;
+
+//Simple Java Program
+public class TC001_Java {
+
+
+    public static void main(String[] args) {
+        System.out.println("This is my first java program");
+    }
+}
