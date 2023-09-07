fe11ced0e83946aeb7c0c18b9438ce07b54fce55
Keyur <keyurshah12345@gmail.com>
2023-09-07 07:39:19 +0530

    new changes

--- Java_100_Programs/TC002_Java.java
+++ Java_100_Programs/TC002_Java.java
@@ -1,2 +1,14 @@
-package Java_100_Programs;public class TC002_Java {
+//Print Integer in java
+
+package Java_100_Programs;
+
+public class TC002_Java {
+static int c;
+    public static void main(String[] args) {
+
+        System.out.println("Value of integer c when not intialize will be   "+ c);
+
+
+    }
+
 }
